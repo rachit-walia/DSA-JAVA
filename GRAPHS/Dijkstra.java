c658cc476415cf68baef666f161568e2361980b8
rachit-walia <rachitwalia3@gmail.com>
2024-11-22 09:40:07 +0530

    minor changes in code

--- GRAPHS/Dijkstra.java
+++ GRAPHS/Dijkstra.java
@@ -1,26 +1,35 @@
+
 import java.util.*;
 
-class Dijkstra {
-    ArrayList<Integer> dijkstra(ArrayList<ArrayList<iPair>> adj, int src) {
+class Graph {
 
+    static class Pair {
+
+        int distance;
+        int node;
+
+        Pair(int distance, int node) {
+            this.distance = distance;
+            this.node = node;
+        }
+    }
+
+    static ArrayList<Integer> dijkstra(ArrayList<ArrayList<Pair>> adj, int src) {
         int V = adj.size();
         PriorityQueue<Pair> pq = new PriorityQueue<>(Comparator.comparingInt(x -> x.distance));
-
-        ArrayList<Integer> dist = new ArrayList<>();
-        for (int i = 0; i < V; i++)
-            dist.add(Integer.MAX_VALUE);
+        ArrayList<Integer> dist = new ArrayList<>(Collections.nCopies(V, Integer.MAX_VALUE));
 
         dist.set(src, 0);
         pq.add(new Pair(0, src));
 
         while (!pq.isEmpty()) {
-            int dis = pq.peek().distance;
-            int node = pq.peek().node;
-            pq.remove();
+            Pair current = pq.poll();
+            int dis = current.distance;
+            int node = current.node;
 
-            for (iPair temp : adj.get(node)) {
-                int adjNode = temp.first;
-                int adjWeight = temp.second;
+            for (Pair neighbor : adj.get(node)) {
+                int adjNode = neighbor.node;
+                int adjWeight = neighbor.distance;
 
                 if (dis + adjWeight < dist.get(adjNode)) {
                     dist.set(adjNode, dis + adjWeight);
@@ -32,12 +41,15 @@ class Dijkstra {
         return dist;
     }
 
+    static void addEdge(ArrayList<ArrayList<Pair>> adj, int u, int v, int weight) {
+        adj.get(u).add(new Pair(weight, v));
+        adj.get(v).add(new Pair(weight, u));
+    }
 
     public static void main(String[] args) {
-        
         int V = 5;
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
 
-        ArrayList<ArrayList<iPair>> adj = new ArrayList<>();
         for (int i = 0; i < V; i++) {
             adj.add(new ArrayList<>());
         }
@@ -50,37 +62,11 @@ class Dijkstra {
 
         int src = 0;
 
-        Dijkstra sol = new Dijkstra();
-        ArrayList<Integer> distances = sol.dijkstra(adj, src);
+        ArrayList<Integer> distances = dijkstra(adj, src);
 
         System.out.println("Shortest distances from source " + src + ":");
         for (int i = 0; i < distances.size(); i++) {
             System.out.println("Vertex " + i + ": " + distances.get(i));
         }
-    }
-
-    static void addEdge(ArrayList<ArrayList<iPair>> adj, int u, int v, int weight) {
-        adj.get(u).add(new iPair(v, weight));
-        adj.get(v).add(new iPair(u, weight));
-    }
-}
-
-class iPair {
-    int first;
-    int second;
-
-    iPair(int first, int second) {
-        this.first = first;
-        this.second = second;
-    }
-}
-
-class Pair {
-    int distance;
-    int node;
-
-    Pair(int distance, int node) {
-        this.distance = distance;
-        this.node = node;
     }
 }
